58f0bb6568462094d2327a69abba07cab93a0c6c
Cyteon <129582290+Cyteon@users.noreply.github.com>
2024-12-12 08:18:33 +0000

    restructure

---
+++ lang/src/ast.rs
@@ -0,0 +1,28 @@
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum AST {
+    Comment,
+
+    LetDeclaration {
+        name: Option<String>,
+        value: Box<AST>,
+    },
+
+    Identifer(String),
+
+    Number(i64),
+
+    String(String),
+
+    Null,
+    
+    LParen(Box<AST>),
+
+    RParen(Box<AST>),
+
+    Call {
+        name: String,
+        args: Vec<AST>,
+    },
+}
+
---
+++ lang/src/eval.rs
@@ -0,0 +1,74 @@
+use crate::ast::AST;
+
+use std::collections::HashMap;
+
+pub fn eval(expr: AST, context: &mut HashMap<String, AST>) {
+    match expr {
+        AST::Call { name, args } => {
+            match name.as_str() {
+                "print" => {
+                    if args.len() == 1 {
+                        match args[0].clone() {
+                            AST::String(s) => {
+                                println!("{}", s.replace("\"", ""));
+                            }
+    
+                            AST::Number(n) => {
+                                println!("{}", n);
+                            }
+    
+                            AST::Identifer(name) => {
+                                match context.get(&name) {
+                                    Some(value) => {
+                                        match value {
+                                            AST::String(s) => {
+                                                println!("{}", s.replace("\"", ""));
+                                            }
+    
+                                            AST::Number(n) => {
+                                                println!("{}", n);
+                                            }
+    
+                                            _ => {
+                                                println!("Unknown value: {:?}", value);
+                                            }
+                                        }
+                                    }
+    
+                                    None => {
+                                        println!("Unknown variable: {}", name);
+                                    }
+                                }
+                            }
+    
+                            _ => {
+                                println!("{:?}", args[0]);
+                            }
+                        }
+                    } else {
+                        println!("Expected 1 argument");
+                    }
+                }
+
+                "exit" => {
+                    println!("Exiting...");
+                    std::process::exit(0);
+                }
+
+                _ => {
+                    println!("Unknown function: {}", name);
+                }
+            }
+        }
+
+        AST::LetDeclaration { name, value } => {
+            if let Some(name) = name {
+                context.insert(name, *value);
+            }
+        }
+
+        _ => {
+            println!("Unknown expression: {:?}", expr);
+        }
+    }
+}
