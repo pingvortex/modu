9c06e6d2ac519dfc8dfff5897c98c46eb24de3ea
cyteon <cyteon@hackclub.app>
2025-01-20 21:07:19 +0100

    ffi works, however there is only int support in input atm, todo: string support, float support, docs

---
+++ lang/examples/ffi/ffi_test/src/lib.rs
@@ -0,0 +1,47 @@
+#[unsafe(no_mangle)]
+pub extern "C" fn add(
+    argc: std::ffi::c_int,
+    argv: *const std::ffi::c_int
+) -> i32 {
+    if argc != 2 {
+        panic!("add requires 2 arguments");
+    }
+
+    let args = unsafe {
+        std::slice::from_raw_parts(argv, argc as usize)
+    };
+
+    args[0] + args[1]
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn a(
+    argc: std::ffi::c_int,
+    argv: *mut std::ffi::c_int
+) -> i32 {
+    if argc != 1 {
+        panic!("a requires 1 argument");
+    }
+
+    let args = unsafe {
+        std::slice::from_raw_parts(argv, argc as usize)
+    };
+
+    args[0]
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn one() -> i64 {
+    1
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn string() -> *mut std::ffi::c_char {
+    std::ffi::CString::new("Hello, World!").unwrap().into_raw()
+}
+
+
+#[unsafe(no_mangle)]
+pub extern "C" fn hello_world() {
+    println!("Hello, World!");
+}
