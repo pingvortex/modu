b1d496199e526e95b81f90c9725fa3554226ba2d
Antônio Breno <coisasaletaorias@gmail.com>
2025-03-03 23:18:35 -0300

    More constants and many more functions

--- lang/src/packages/math.rs
+++ lang/src/packages/math.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::f64::NAN;
 use rand;
 
 use crate::ast::AST;
@@ -221,8 +222,320 @@ pub fn cbrt(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), 
         }
 
         _ => {
-            return Err("sqrt requires a number".to_string());
+            return Err("cbrt requires a number".to_string());
         }
+    }
+}
+
+pub fn acos(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a < -1 || a > 1 {
+                return Err("acos requires a number in the interval [-1, 1]".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).acos()), AST::Null));
+        },
+
+        Ok(AST::Float(a)) => {
+            if a < -1.0 || a > 1.0 {
+                return Err("acos requires a number in the interval [-1, 1]".to_string());
+            }
+
+            return Ok((AST::Float(a.acos()), AST::Null));
+        },
+
+        _ => Err("acos requires a number".to_string())
+    }
+}
+
+pub fn acosh(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a < 1 {
+                return Err("acosh requires a number greater than or equal to 1".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).acosh()), AST::Null));
+        },
+
+        Ok(AST::Float(a)) => {
+            if a < 1.0 {
+                return Err("acosh requires a number greater than or equal to 1".to_string());
+            }
+
+            return Ok((AST::Float(a.acosh()), AST::Null));
+        },
+
+        _ => Err("acosh requires a number".to_string())
+    }
+}
+
+pub fn asin(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a < -1 || a > 1 {
+                return Err("asin requires a number in the interval [-1, 1]".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).asin()), AST::Null));
+        },
+
+        Ok(AST::Float(a)) => {
+            if a < -1.0 || a > 1.0 {
+                return Err("asin requires a number in the interval [-1, 1]".to_string());
+            }
+
+            return Ok((AST::Float(a.asin()), AST::Null));
+        },
+
+        _ => Err("asin requires a number".to_string())
+    }
+}
+
+pub fn asinh(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).asinh()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.asinh()), AST::Null)),
+
+        _ => Err("asinh requires a number".to_string())
+    }
+}
+
+pub fn atan(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).atan()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.atan()), AST::Null)),
+
+        _ => Err("atan requires a number".to_string())
+    }
+}
+
+pub fn atanh(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a < -1 || a > 1 {
+                return Err("atanh requires a number in the interval [-1, 1]".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).atanh()), AST::Null));
+        },
+
+        Ok(AST::Float(a)) => {
+            if a < -1.0 || a > 1.0 {
+                return Err("atanh requires a number in the interval [-1, 1]".to_string());
+            }
+
+            return Ok((AST::Float(a.atanh()), AST::Null));
+        },
+
+        _ => Err("atanh requires a number".to_string())
+    }
+}
+
+pub fn cos(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a < -1 || a > 1 {
+                return Err("cos requires a number in the interval [-1, 1]".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).cos()), AST::Null));
+        },
+
+        Ok(AST::Float(a)) => {
+            if a < -1.0 || a > 1.0 {
+                return Err("cos requires a number in the interval [-1, 1]".to_string());
+            }
+
+            return Ok((AST::Float(a.cos()), AST::Null));
+        },
+
+        _ => Err("cos requires a number".to_string())
+    }
+}
+
+pub fn cosh(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).cosh()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.cosh()), AST::Null)),
+
+        _ => Err("cosh requires a number".to_string())
+    }
+}
+
+pub fn exp(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).exp()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.exp()), AST::Null)),
+
+        _ => Err("exp requires a number".to_string())
+    }
+}
+
+pub fn exp2(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).exp2()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.exp2()), AST::Null)),
+
+        _ => Err("exp2 requires a number".to_string())
+    }
+}
+
+pub fn expm1(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).exp_m1()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.exp_m1()), AST::Null)),
+
+        _ => Err("expm1 requires a number".to_string())
+    }
+}
+
+pub fn fract(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).fract()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.fract()), AST::Null)),
+
+        _ => Err("fract requires a number".to_string())
+    }
+}
+
+pub fn ln(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a <= 0 {
+                return Err("ln requires a number greater than 0".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).ln()), AST::Null));
+        },
+
+        Ok(AST::Float(a)) => {
+            if a <= 0.0 {
+                return Err("ln requires a number greater than 0".to_string());
+            }
+
+            return Ok((AST::Float(a.ln()), AST::Null));
+        },
+
+        _ => Err("ln requires a number".to_string())
+    }
+}
+
+pub fn ln1p(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a <= -1 {
+                return Err("ln1p requires a number greater than -1".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).ln_1p()), AST::Null));
+        }
+
+        Ok(AST::Float(a)) => {
+            if a <= -1.0 {
+                return Err("ln1p requires a number greater than -1".to_string());
+            }
+
+            return Ok((AST::Float(a.ln_1p()), AST::Null));
+        }
+
+        _ => Err("ln1p requires a number".to_string())
+    }
+}
+
+pub fn log10(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a <= 0 {
+                return Err("log10 requires a number greater than 0".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).log10()), AST::Null));
+        },
+
+        Ok(AST::Float(a)) => {
+            if a <= 0.0 {
+                return Err("log10 requires a number greater than 0".to_string());
+            }
+
+            return Ok((AST::Float(a.log10()), AST::Null));
+        },
+
+        _ => Err("log10 requires a number".to_string())
+    }
+}
+
+pub fn log2(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a <= 0 {
+                return Err("log2 requires a number greater than 0".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).log2()), AST::Null));
+        },
+
+        Ok(AST::Float(a)) => {
+            if a <= 0.0 {
+                return Err("log2 requires a number greater than 0".to_string());
+            }
+
+            return Ok((AST::Float(a.log2()), AST::Null));
+        },
+
+        _ => Err("log2 requires a number".to_string())
+    }
+}
+
+pub fn sin(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).sin()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.sin()), AST::Null)),
+
+        _ => Err("sin requires a number".to_string())
+    }
+}
+
+pub fn sinh(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).sinh()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.sinh()), AST::Null)),
+
+        _ => Err("sinh requires a number".to_string())
+    }
+}
+
+pub fn tan(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).tan()), AST::Null)),
+        Ok(AST::Float(a)) => {
+            let b = a.tan();
+            if b.is_nan() {
+                return Err("tan requires a real number that is not an odd multiple of pi/2".to_string());
+            }
+            Ok((AST::Float(b), AST::Null))
+        },
+
+        _ => Err("tan requires a number".to_string())
+    }
+}
+
+pub fn tanh(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).tanh()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.tanh()), AST::Null)),
+
+        _ => Err("tanh requires a number".to_string())
+    }
+}
+
+pub fn trunc(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => Ok((AST::Float((a as f64).trunc()), AST::Null)),
+        Ok(AST::Float(a)) => Ok((AST::Float(a.trunc()), AST::Null)),
+
+        _ => Err("trunc requires a number".to_string())
     }
 }
 
@@ -317,6 +630,205 @@ pub fn get_object() -> HashMap<String, AST> {
             args: vec!["a".to_string()],
             call_fn: cbrt,
         }
+    );
+
+    objects.insert(
+        "acos".to_string(),
+        AST::InternalFunction {
+            name: "acos".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: acos,
+        }
+    );
+
+    objects.insert(
+        "acosh".to_string(),
+        AST::InternalFunction {
+            name: "acosh".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: acosh,
+        }
+    );
+
+    objects.insert(
+        "asin".to_string(),
+        AST::InternalFunction {
+            name: "asin".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: asin,
+        }
+    );
+
+    objects.insert(
+        "asinh".to_string(),
+        AST::InternalFunction {
+            name: "asinh".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: asinh,
+        }
+    );
+
+    objects.insert(
+        "atan".to_string(),
+        AST::InternalFunction {
+            name: "atan".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: atan,
+        }
+    );
+
+    objects.insert(
+        "atanh".to_string(),
+        AST::InternalFunction {
+            name: "atanh".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: atanh,
+        }
+    );
+
+    objects.insert(
+        "cos".to_string(),
+        AST::InternalFunction {
+            name: "cos".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: cos,
+        }
+    );
+
+    objects.insert(
+        "cosh".to_string(),
+        AST::InternalFunction {
+            name: "cosh".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: cosh,
+        }
+    );
+
+    objects.insert(
+        "exp".to_string(),
+        AST::InternalFunction {
+            name: "exp".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: exp,
+        }
+    );
+
+    objects.insert(
+        "exp2".to_string(),
+        AST::InternalFunction {
+            name: "exp2".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: exp2,
+        }
+    );
+
+    objects.insert(
+        "expm1".to_string(),
+        AST::InternalFunction {
+            name: "expm1".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: expm1,
+        }
+    );
+
+    objects.insert(
+        "fract".to_string(),
+        AST::InternalFunction {
+            name: "fract".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: fract,
+        }
+    );
+
+    objects.insert(
+        "ln".to_string(),
+        AST::InternalFunction {
+            name: "ln".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: ln,
+        }
+    );
+
+    objects.insert(
+        "ln1p".to_string(),
+        AST::InternalFunction {
+            name: "ln1p".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: ln1p,
+        }
+    );
+
+    objects.insert(
+        "log10".to_string(),
+        AST::InternalFunction {
+            name: "log10".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: log10,
+        }
+    );
+
+    objects.insert(
+        "log2".to_string(),
+        AST::InternalFunction {
+            name: "log2".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: log2,
+        }
+    );
+
+    objects.insert(
+        "sin".to_string(),
+        AST::InternalFunction {
+            name: "sin".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: sin,
+        }
+    );
+
+    objects.insert(
+        "sinh".to_string(),
+        AST::InternalFunction {
+            name: "sinh".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: sinh,
+        }
+    );
+
+    objects.insert(
+        "tan".to_string(),
+        AST::InternalFunction {
+            name: "tan".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: tan,
+        }
+    );
+
+    objects.insert(
+        "tanh".to_string(),
+        AST::InternalFunction {
+            name: "tanh".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: tanh,
+        }
+    );
+
+    objects.insert(
+        "trunc".to_string(),
+        AST::InternalFunction {
+            name: "trunc".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: trunc,
+        }
+    );
+
+    objects.insert(
+        "CBRT_2".to_string(),
+        AST::Float(1.25992104989487316476721060727822835_f64)
+    );
+
+    objects.insert(
+        "CBRT_3".to_string(),
+        AST::Float(1.44224957030740838232163831078010958_f64)
     );
 
     objects.insert(
@@ -440,6 +952,21 @@ pub fn get_object() -> HashMap<String, AST> {
     );
 
     objects.insert(
+        "RT12_2".to_string(),
+        AST::Float(1.05946309435929526456182529494634170_f64)
+    );
+
+    objects.insert(
+        "SILVER".to_string(),
+        AST::Float(2.41421356237309504880168872420969808_f64)
+    );
+
+    objects.insert(
+        "SUPERPHI".to_string(),
+        AST::Float(1.46557123187676802665673122521993910_f64)
+    );
+
+    objects.insert(
         "SQRT_2".to_string(),
         AST::Float(1.41421356237309504880168872420969808_f64)
     );
@@ -470,7 +997,7 @@ mod tests {
     fn get_object_test() {
         let object = get_object();
 
-        assert_eq!(object.len(), 38);
+        assert_eq!(object.len(), 64);
         assert_eq!(object.contains_key("div"), true);
     }
 
