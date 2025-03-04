d95d10843c4401f366e1f51bd5e2deb084b6f43d
Antônio Breno <coisasaletaorias@gmail.com>
2025-03-03 21:50:49 -0300

    Added cbrt and many constants

--- lang/src/packages/math.rs
+++ lang/src/packages/math.rs
@@ -202,11 +202,35 @@ pub fn random_int(_: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST
     return Ok((AST::Number(rand::random()), AST::Null));
 }
 
+pub fn cbrt(args: Vec<AST>, _: &mut HashMap<String, AST>) -> Result<(AST, AST), String> {
+    match eval(args[0].clone(), &mut HashMap::new()) {
+        Ok(AST::Number(a)) => {
+            if a < 0 {
+                return Err("cbrt requires a positive number".to_string());
+            }
+
+            return Ok((AST::Float((a as f64).cbrt()), AST::Null));
+        }
+
+        Ok(AST::Float(a)) => {
+            if a < 0.0 {
+                return Err("cbrt requires a positive number".to_string());
+            }
+
+            return Ok((AST::Float(a.cbrt()), AST::Null));
+        }
+
+        _ => {
+            return Err("sqrt requires a number".to_string());
+        }
+    }
+}
+
 pub fn get_object() -> HashMap<String, AST> {
     let mut objects = HashMap::new();
 
     objects.insert(
-        "div".to_string(), 
+        "div".to_string(),
         AST::InternalFunction {
             name: "div".to_string(),
             args: vec!["a".to_string(), "b".to_string()],
@@ -215,7 +239,7 @@ pub fn get_object() -> HashMap<String, AST> {
     );
 
     objects.insert(
-        "mul".to_string(), 
+        "mul".to_string(),
         AST::InternalFunction {
             name: "mul".to_string(),
             args: vec!["a".to_string(), "b".to_string()],
@@ -224,7 +248,7 @@ pub fn get_object() -> HashMap<String, AST> {
     );
 
     objects.insert(
-        "abs".to_string(), 
+        "abs".to_string(),
         AST::InternalFunction {
             name: "abs".to_string(),
             args: vec!["a".to_string()],
@@ -233,7 +257,7 @@ pub fn get_object() -> HashMap<String, AST> {
     );
 
     objects.insert(
-        "sqrt".to_string(), 
+        "sqrt".to_string(),
         AST::InternalFunction {
             name: "sqrt".to_string(),
             args: vec!["a".to_string()],
@@ -242,7 +266,7 @@ pub fn get_object() -> HashMap<String, AST> {
     );
 
     objects.insert(
-        "pow".to_string(), 
+        "pow".to_string(),
         AST::InternalFunction {
             name: "pow".to_string(),
             args: vec!["a".to_string(), "b".to_string()],
@@ -251,7 +275,7 @@ pub fn get_object() -> HashMap<String, AST> {
     );
 
     objects.insert(
-        "floor".to_string(), 
+        "floor".to_string(),
         AST::InternalFunction {
             name: "floor".to_string(),
             args: vec!["a".to_string()],
@@ -260,7 +284,7 @@ pub fn get_object() -> HashMap<String, AST> {
     );
 
     objects.insert(
-        "ceil".to_string(), 
+        "ceil".to_string(),
         AST::InternalFunction {
             name: "ceil".to_string(),
             args: vec!["a".to_string()],
@@ -287,8 +311,152 @@ pub fn get_object() -> HashMap<String, AST> {
     );
 
     objects.insert(
+        "cbrt".to_string(),
+        AST::InternalFunction {
+            name: "cbrt".to_string(),
+            args: vec!["a".to_string()],
+            call_fn: cbrt,
+        }
+    );
+
+    objects.insert(
+        "E".to_string(),
+        AST::Float(2.71828182845904523536028747135266250_f64)
+    );
+
+    objects.insert(
+        "EGAMMA".to_string(),
+        AST::Float(0.57721566490153286060651209008240243_f64)
+    );
+
+    objects.insert(
+        "FRAC_1_PI".to_string(),
+        AST::Float(0.31830988618379067153776752674502872_f64)
+    );
+
+    objects.insert(
+        "FRAC_1_SQRT_2".to_string(),
+        AST::Float(0.70710678118654752440084436210484903_f64)
+    );
+
+    objects.insert(
+        "FRAC_1_SQRT_2PI".to_string(),
+        AST::Float(0.39894228040143267793994605993438186_f64)
+    );
+
+    objects.insert(
+        "FRAC_1_SQRT_3".to_string(),
+        AST::Float(0.57735026918962576450914878050195745_f64)
+    );
+
+    objects.insert(
+        "FRAC_1_SQRT_PI".to_string(),
+        AST::Float(0.56418958354775628694807945156077258_f64)
+    );
+
+    objects.insert(
+        "FRAC_2_PI".to_string(),
+        AST::Float(0.63661977236758134307553505349005744_f64)
+    );
+
+    objects.insert(
+        "FRAC_2_SQRT_PI".to_string(),
+        AST::Float(1.12837916709551257389615890312154517_f64)
+    );
+
+    objects.insert(
+        "FRAC_PI_2".to_string(),
+        AST::Float(1.57079632679489661923132169163975144_f64)
+    );
+
+    objects.insert(
+        "FRAC_PI_3".to_string(),
+        AST::Float(1.04719755119659774615421446109316763_f64)
+    );
+
+    objects.insert(
+        "FRAC_PI_4".to_string(),
+        AST::Float(0.78539816339744830961566084581987572_f64)
+    );
+
+    objects.insert(
+        "FRAC_PI_5".to_string(),
+        AST::Float(0.62831853071795864769252867665590057_f64)
+    );
+
+    objects.insert(
+        "FRAC_PI_6".to_string(),
+        AST::Float(0.52359877559829887307710723054658381_f64)
+    );
+
+    objects.insert(
+        "FRAC_PI_7".to_string(),
+        AST::Float(0.44879895051282760549466334046850041_f64)
+    );
+
+    objects.insert(
+        "FRAC_PI_8".to_string(),
+        AST::Float(0.39269908169872415480783042290993786_f64)
+    );
+
+    objects.insert(
+        "LN_2".to_string(),
+        AST::Float(0.69314718055994530941723212145817656_f64)
+    );
+
+    objects.insert(
+        "LN_10".to_string(),
+        AST::Float(2.30258509299404568401799145468436421_f64)
+    );
+
+    objects.insert(
+        "LOG2_10".to_string(),
+        AST::Float(3.32192809488736234787031942948939018_f64)
+    );
+
+    objects.insert(
+        "LOG2_E".to_string(),
+        AST::Float(1.44269504088896340735992468100189214_f64)
+    );
+
+    objects.insert(
+        "LOG10_2".to_string(),
+        AST::Float(0.30102999566398119521373889472449302_f64)
+    );
+
+    objects.insert(
+        "LOG10_E".to_string(),
+        AST::Float(0.43429448190325182765112891891660508_f64)
+    );
+
+    objects.insert(
+        "PHI".to_string(),
+        AST::Float(1.61803398874989484820458683436563811_f64)
+    );
+
+    objects.insert(
         "PI".to_string(),
-        AST::Float(std::f64::consts::PI)
+        AST::Float(3.14159265358979323846264338327950288_f64)
+    );
+
+    objects.insert(
+        "SQRT_2".to_string(),
+        AST::Float(1.41421356237309504880168872420969808_f64)
+    );
+
+    objects.insert(
+        "SQRT_3".to_string(),
+        AST::Float(1.73205080756887729352744634150587236_f64)
+    );
+
+    objects.insert(
+        "SQRT_5".to_string(),
+        AST::Float(2.23606797749978969640917366873127623_f64)
+    );
+
+    objects.insert(
+        "TAU".to_string(),
+        AST::Float(6.28318530717958647692528676655900577_f64)
     );
 
     objects
@@ -302,7 +470,7 @@ mod tests {
     fn get_object_test() {
         let object = get_object();
 
-        assert_eq!(object.len(), 10);
+        assert_eq!(object.len(), 38);
         assert_eq!(object.contains_key("div"), true);
     }
 
